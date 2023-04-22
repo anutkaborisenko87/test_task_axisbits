ee09af111939961c940d7290c2c1d6b18af32733
Anutka Borisenko <anutkaborisenko87@gmail.com>
2023-04-22 21:27:44 +0300

    last fix

--- task_3_6/solution_task_3.sql
+++ task_3_6/solution_task_3.sql
@@ -3,7 +3,7 @@ CREATE TABLE categories (
                             name VARCHAR(255) NOT NULL,
                             parent_id INT(11) UNSIGNED DEFAULT NULL,
                             PRIMARY KEY (id),
-                            FOREIGN KEY (parent_id) REFERENCES categories(id) ON DELETE CASCADE
+                            FOREIGN KEY (parent_id) REFERENCES categories(id) ON DELETE SET NULL
 );
 
 CREATE TABLE products (
